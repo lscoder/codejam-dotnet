f3f5921cb730dfbd82c49ae2af1cb2a8abe4954d
Leonardo Campos <u06lxc@macys.com>
2014-04-12 11:49:35 -0300

    Solution - 2014-QB

--- src/LSCoder.CodeJam.ConsoleProcessor/Program.cs
+++ src/LSCoder.CodeJam.ConsoleProcessor/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Threading;
 
 namespace LSCoder.CodeJam.ConsoleProcessor
 {
@@ -10,6 +12,8 @@ namespace LSCoder.CodeJam.ConsoleProcessor
     {
         static void Main(string[] args)
         {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-us");
+
             var fileManager = new FileManager();
             var inputFileSelector = new AsciiMenuFileSelector(fileManager, Console.In, Console.Out);
 
@@ -25,13 +29,18 @@ namespace LSCoder.CodeJam.ConsoleProcessor
             inputFile.Dispose();
             outputFile.Dispose();
 
+            Trace.WriteLine(string.Format("\nFile `{0}` created!", problemInputFile.Name));
+
             Console.ReadKey(true);
         }
 
         private static void Solve(TextReader inputFile, TextWriter outputFile)
         {
-            var testCaseCount = Int32.Parse(inputFile.ReadLine());
-            for(var i = 1; i <= testCaseCount; i++)
+            var testCasesCount = Int32.Parse(inputFile.ReadLine());
+
+            Trace.WriteLine(string.Format("\nRunning `{0}` test cases...\n", testCasesCount));
+
+            for (var i = 1; i <= testCasesCount; i++)
             {
                 SolveTestCase(i, inputFile, outputFile);
             }
@@ -39,11 +48,30 @@ namespace LSCoder.CodeJam.ConsoleProcessor
 
         private static void SolveTestCase(int testCaseId, TextReader inputFile, TextWriter outputFile)
         {
-            var result = "";
+            var tokens = inputFile.ReadLine().Split(' ').Select(Double.Parse).ToList();
 
-            /************************** YOUR CODE **************************/
+            var cps = 2.0;
+            var c = tokens[0];
+            var f = tokens[1];
+            var x = tokens[2];
+            var best = Double.MaxValue;
+            double elapsedTime = 0;
 
-            WriteResult(testCaseId, result, outputFile);
+            do
+            {
+                var tc = c/cps;
+                var tx = x/cps;
+                var current = elapsedTime + tx;
+
+                if(current > best)
+                    break;
+
+                best = elapsedTime + tx;
+                elapsedTime += tc;
+                cps += f;
+            } while (true);
+
+            WriteResult(testCaseId, best.ToString(), outputFile);
         }
 
         private static void WriteResult(int testCaseId, string result, TextWriter outputFile)
