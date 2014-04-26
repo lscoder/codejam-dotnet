4bebda524a8399949167048d8ada5b18fe0e911f
Leonardo Campos <u06lxc@macys.com>
2014-04-26 00:22:38 -0300

    2014-1A-A Solution

--- src/LSCoder.CodeJam.ConsoleProcessor/Program.cs
+++ src/LSCoder.CodeJam.ConsoleProcessor/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Threading;
 
 namespace LSCoder.CodeJam.ConsoleProcessor
@@ -47,9 +48,95 @@ namespace LSCoder.CodeJam.ConsoleProcessor
 
         private static string Solve(TextReader inputFile)
         {
-            /************************** YOUR CODE HERE **************************/
+            var nl = inputFile.ReadLine().Split(' ').Select(Int32.Parse).ToArray();
+            var n = nl[0];
+            var l = nl[1];
+            var initial = ConvertFlow(inputFile.ReadLine().Split(' '));
+            var final = ConvertFlow(inputFile.ReadLine().Split(' '));
 
-            return "Your result here";
+            var result = Solve(n, l, initial, final, 0);
+
+            return result == -1 ? "NOT POSSIBLE" : result.ToString();
+        }
+
+        public static int Solve(int n, int l, long[] initial, long[] final, int pos)
+        {
+            if (!IsOk(initial, final, pos))
+                return -1;
+
+            if (pos == l)
+                return 0;
+
+
+            var r1 = Solve(n, l, initial, final, pos + 1);
+
+            if (r1 == 0)
+                return 0;
+
+            SwitchBit(initial, pos);
+            var r2 = Solve(n, l, initial, final, pos + 1);
+            SwitchBit(initial, pos);
+
+            if ((r1 == -1) && (r2 == -1))
+                return -1;
+
+            if ((r1 != -1) && (r2 != -1))
+            {
+                if (r1 <= r2)
+                    return r1;
+
+                return 1 + r2;
+            }
+
+            if (r1 != -1)
+                return r1;
+
+            return 1 + r2;
+        }
+
+        private static long[] ConvertFlow(string[] input)
+        {
+            var result = new long[input.Length];
+
+            for (var i = 0; i < input.Length; i++)
+                result[i] = Convert.ToInt64(input[i], 2);
+
+            return result;
+        }
+
+        private static void SwitchBit(long[] input, int bit)
+        {
+            for (var i = 0; i < input.Length; i++)
+            {
+                input[i] = input[i] ^ (1 << bit);
+            }
+        }
+
+        private static bool IsOk(long[] start, long[] end, int bitCount)
+        {
+            if (bitCount == 0)
+                return true;
+
+            long mask = (1 << bitCount) - 1;
+            var s = new long[start.Length];
+            var e = new long[end.Length];
+
+            for (int i = 0; i < start.Length; i++)
+            {
+                s[i] = start[i] & mask;
+                e[i] = end[i] & mask;
+            }
+
+            Array.Sort(s);
+            Array.Sort(e);
+
+            for (int i = 0; i < start.Length; i++)
+            {
+                if (s[i] != e[i])
+                    return false;
+            }
+
+            return true;
         }
 
         private static void WriteResult(int testCaseId, string result, TextWriter outputFile)
