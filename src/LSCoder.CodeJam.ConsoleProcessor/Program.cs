f4b633c6ab3f795b12c0aa10d9e781bc983f7f9f
Leonardo Campos <u06lxc@macys.com>
2014-05-03 14:33:08 -0300

    2014-1B-A Solution

--- src/LSCoder.CodeJam.ConsoleProcessor/Program.cs
+++ src/LSCoder.CodeJam.ConsoleProcessor/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading;
 
 namespace LSCoder.CodeJam.ConsoleProcessor
@@ -47,9 +48,88 @@ namespace LSCoder.CodeJam.ConsoleProcessor
 
         private static string Solve(Scanner scanner)
         {
-            /************************** YOUR CODE HERE **************************/
+            var count = scanner.ReadInt();
+            var lines = new string[count];
+            var invalid = false;
 
-            return "Your result here";
+            var baseStr = "";
+            for (var i = 0; i < count; i++)
+            {
+                var line = scanner.ReadLine();
+                lines[i] = line;
+
+                var singleLetters = Regex.Replace(lines[i], @"([a-z])\1+", @"$1");
+
+                if (i == 0)
+                    baseStr = singleLetters;
+                else if (singleLetters != baseStr)
+                    invalid = true;
+            }
+
+            if(invalid)
+                return "Fegla Won";
+
+            var chs = new int[count, baseStr.Length];
+            for (int i = 0; i < count; i++)
+            {
+                var line = lines[i];
+
+                var idx = 0;
+                var chIndex = 0;
+                while (idx < line.Length)
+                {
+                    var counter = 1;
+                    idx++;
+
+                    while (idx < line.Length)
+                    {
+                        if(line[idx] == line[idx - 1])
+                        {
+                            idx++;
+                            counter++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    chs[i, chIndex++] = counter;
+                }
+            }
+
+
+            var moves = 0;
+
+            for (int chIdx = 0; chIdx < baseStr.Length; chIdx++)
+            {
+                int min = chs[0, chIdx];
+                int max = chs[0, chIdx];
+
+                for (int i = 0; i < count; i++)
+                {
+                    if (chs[i, chIdx] < min)
+                        min = chs[i, chIdx];
+                    if (chs[i, chIdx] > max)
+                        max = chs[i, chIdx];
+                }
+
+                var minMove = Int32.MaxValue;
+                for (var j = min; j <= max; j++)
+                {
+                    var m = 0;
+                    for (var k = 0; k < count; k++)
+                    {
+                        m += Math.Abs(j - chs[k, chIdx]);
+                    }
+
+                    minMove = Math.Min(minMove, m);
+                }
+
+                moves += minMove;
+            }
+
+            return moves.ToString();
         }
 
         private static void WriteResult(int testCaseId, string result, TextWriter outputFile)
